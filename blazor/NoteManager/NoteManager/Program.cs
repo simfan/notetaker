d4bec367546ccf4bbbc8c9f14062c8a0df3ad603
Chris Glock <cglock@signaturechampions.com>
2026-03-09 15:26:40 -0500

    near real-time update

--- blazor/NoteManager/NoteManager/Program.cs
+++ blazor/NoteManager/NoteManager/Program.cs
@@ -10,16 +10,23 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient());
 builder.Services.AddScoped<SupabaseService>();
-builder.Services.AddSingleton(provider =>
+/*builder.Services.AddSingleton(provider =>
 {
     var config = provider.GetRequiredService<IConfiguration>();
-    var url = config.["Supabase:Url"]!;
+    var url = config["Supabase:Url"]!;
     var key = config["Supabase:AnonKey"]!;
     return new Supabase.Client(url, key, new Supabase.SupabaseOptions
     {
         AutoConnectRealtime = true,
     });
-});
+});*/
 builder.Services.AddBlazorStrap();
 
 await builder.Build().RunAsync();
+/*var host = builder.Build();
+
+// Initialize Supabase client (triggers AutoConnectRealtime)
+var supabase = host.Services.GetRequiredService<Supabase.Client>();
+await supabase.InitializeAsync();
+
+await host.RunAsync();*/
